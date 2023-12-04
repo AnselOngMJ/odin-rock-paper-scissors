c0c0612bcf13869115b2a1d95095709cb5d3698e
AnselOngMJ <111511997+AnselOngMJ@users.noreply.github.com>
2023-12-04 12:39:40 +0000

    Create function to play a round of the game given inputs

--- javascript.js
+++ javascript.js
@@ -2,4 +2,27 @@ function getComputerChoice() {
     let choices = ['Rock', 'Paper', 'Scissors'];
     return choices[Math.floor(Math.random() * choices.length)];
 }
-console.log(getComputerChoice());
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+    computerSelection = computerSelection;
+    if (playerSelection === computerSelection) {
+        return 'Tie!';
+    }
+    switch (playerSelection + computerSelection) {
+        case 'RockScissors':
+        case 'PaperRock':
+        case 'ScissorsPaper':
+            return `You Win! ${playerSelection} beats ${computerSelection}`;
+        case 'RockPaper':
+        case 'PaperScissors':
+        case 'ScissorsRock':
+            return `You Lose! ${computerSelection} beats ${playerSelection}`;
+        default:
+            return 'Invalid input!';
+    }
+}
+
+const playerSelection = prompt();
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
